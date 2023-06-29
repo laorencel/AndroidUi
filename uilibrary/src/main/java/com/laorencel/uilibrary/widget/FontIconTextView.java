02df0056907c763b12dbe0ad9228ca31b85542ba
laorencel <laorencel@163.com>
2023-06-29 12:08:04 +0800

    FontIconTextView添加自定义资源属性；

--- uilibrary/src/main/java/com/laorencel/uilibrary/widget/FontIconTextView.java
+++ uilibrary/src/main/java/com/laorencel/uilibrary/widget/FontIconTextView.java
@@ -2,6 +2,7 @@ package com.laorencel.uilibrary.widget;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -9,30 +10,33 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 import com.laorencel.uilibrary.R;
+import com.laorencel.uilibrary.util.EmptyUtil;
 
 /**
  * 简单封装，在代码中使用时就不需要再设置Typeface了
  */
 @SuppressLint("AppCompatCustomView")
 public class FontIconTextView extends TextView {
+    private String typefaceAsset = "iconfont.ttf";
+
     public FontIconTextView(Context context) {
         super(context);
-        init(context);
+        init(context, null);
     }
 
     public FontIconTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context, attrs);
     }
 
     public FontIconTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context, attrs);
     }
 
     public FontIconTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context);
+        init(context, attrs);
     }
 
     /**
@@ -40,10 +44,19 @@ public class FontIconTextView extends TextView {
      *
      * @param context
      */
-    private void init(Context context) {
-        //设置字体图标
-        Typeface font = Typeface.createFromAsset(context.getAssets(), "iconfont.ttf");
-        this.setTypeface(font);
+    private void init(Context context, @Nullable AttributeSet attrs) {
+        if (null != attrs) {
+            try (TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.FontIconTextView)) {
+                typefaceAsset = ta.getString(R.styleable.FontIconTextView_typefaceAsset);
+                ta.recycle();
+            }
+        }
+        if (!EmptyUtil.isEmpty(typefaceAsset)) {
+            //设置字体图标
+            Typeface font = Typeface.createFromAsset(context.getAssets(), typefaceAsset);
+            this.setTypeface(font);
+        }
+
 //        setText(getResources().getString(R.string.font_icon_add));
 
         //正常的写法（只是代码中，xml中不需要）
