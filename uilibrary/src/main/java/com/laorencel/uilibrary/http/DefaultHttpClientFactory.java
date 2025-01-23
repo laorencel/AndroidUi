a9b5d0d92c60f21643d6d121e5380f37fb49b83c
laorencel <laorencel@163.com>
2025-01-23 10:45:46 +0800

    修改DefaultHttpClientFactory；

--- uilibrary/src/main/java/com/laorencel/uilibrary/http/DefaultHttpClientFactory.java
+++ uilibrary/src/main/java/com/laorencel/uilibrary/http/DefaultHttpClientFactory.java
@@ -20,7 +20,6 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 /**
  * 默认的网络请求工厂，作为一个实例，实际项目中可以按此编写
- *
  */
 public class DefaultHttpClientFactory {
 
@@ -39,6 +38,7 @@ public class DefaultHttpClientFactory {
     private Map<String, Object> clientMap = new HashMap<>();
 
     private Gson gson;
+
     public Object getHttpClient(String key) {
         Object api = clientMap.get(key);
         return api;
@@ -58,19 +58,19 @@ public class DefaultHttpClientFactory {
     }
 
     public <T> T create(Class<T> apiClass, String baseUrl, List<Converter.Factory> factories, List<Interceptor> interceptors) {
-        Object obj = clientMap.get(baseUrl);
+        Object obj = clientMap.get(apiClass.getName() + "_" + baseUrl);
 
         if (null != obj) {
             return (T) obj;
         }
 
         T api = RetrofitClient.get().createRetrofit(baseUrl, factories, interceptors).create(apiClass);
-        clientMap.put(baseUrl, api);
+        clientMap.put(apiClass.getName() + "_" + baseUrl, api);
         return api;
     }
 
     private Gson getGson() {
-        if (null == gson){
+        if (null == gson) {
             gson = new GsonBuilder()
                     .setLenient()
 //                .registerTypeAdapter(long.class, LongTypeAdapter)
