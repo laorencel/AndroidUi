78a0e6552a24d5fd4c36041d0ee428b1630b6906
laorencel <laorencel@163.com>
2025-06-20 15:27:21 +0800

    修改主题配置；

--- app/src/main/java/com/laorencel/ui/UiApplication.java
+++ app/src/main/java/com/laorencel/ui/UiApplication.java
@@ -20,7 +20,7 @@ public class UiApplication extends Application {
         SmartRefreshLayout.setDefaultRefreshHeaderCreator(new DefaultRefreshHeaderCreator() {
             @Override
             public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
-                layout.setPrimaryColorsId(com.laorencel.uilibrary.R.color.md_theme_light_primary, com.laorencel.uilibrary.R.color.white);//全局设置主题颜色
+                layout.setPrimaryColorsId(com.google.android.material.R.color.design_default_color_primary, com.laorencel.uilibrary.R.color.white);//全局设置主题颜色
                 return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
             }
         });
